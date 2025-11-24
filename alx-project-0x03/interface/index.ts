146555238c5307ef8df2169cb8b377cb07a9c62b
PleasureKemefa <80639745+PleasureKemefa@users.noreply.github.com>
2025-11-23 18:27:44 -0800

    Add files via upload

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,17 @@
+
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+
+export interface ButtonProps {
+    buttonLabel: string
+    buttonSize?: string
+    buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+    action?: () => void
+}
+
+export interface PageRouteProps {
+  pageRoute: string
+}
